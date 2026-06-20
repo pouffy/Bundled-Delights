76a85bec898f096ec8944ace860ce6fa35d2d2db
PouffyDev <99536749+pouffy@users.noreply.github.com>
2026-06-20 23:25:48 +0100

    Start port to 1.21.1

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.minecraftforge.net/")
+        maven("https://maven.parchmentmc.org")
+        maven("https://repo.spongepowered.org/repository/maven-public/")
+        maven("https://maven.neoforged.net/releases")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "org.spongepowered.mixin") {
+                useModule("org.spongepowered:mixingradle:0.7-SNAPSHOT")
+            }
+        }
+    }
+}
+
+rootProject.name = "Bundled Delight"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
